cce102fe047f0663b05e6348ef88eb2ff200c48e
Aleksander Petrov <apetrovdev@gmail.com>
2022-01-08 14:38:06 +0200

    router config and axios added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
-import ClassComponent from "./components/ClassComponent";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
+import NavBar from "./components/navbar/NavBar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import TVSeries from "./pages/TVSeries";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const routes = [
+  {
+    path: '/',
+    component: <Home/>,
+    exact: true,
+  },
+  {
+    path: 'movies',
+    component: <Movies/>,
+  },
+  {
+    path: 'tvseries',
+    component: <TVSeries/>,
+  }
+]
 
 function App() {
+
+  const getRoutes = () => {
+    return routes.map((route, index) => {
+      return <Route 
+        key={index}
+        exact={route.exact}
+        path={route.path}
+        element={route.component}
+        />
+    })
+  }
+
   return (
-    <Container>
-      <Row className="mt-5">
-        <Col md={4}>
-          <ClassComponent lastName="Petrov"/>
-          <ClassComponent lastName="Ivanov"/>
-          <ClassComponent lastName="Georgiev"/>
-        </Col>
-      </Row>
-    </Container>
+    <Router>
+      <NavBar/>
+      <Container>
+        <Row className="mt-5">
+          <Routes>
+            {getRoutes()}
+          </Routes>
+        </Row>
+      </Container>
+  </Router>
   );
 }
 
